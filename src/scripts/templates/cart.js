e34e199593f9f0a6f0b63eaa2a9014e378ad1de0
Brian Berg <brian.berg@bvaccel.com>
2019-07-24 11:21:57 -0400

    Bootcamp

--- src/scripts/templates/cart.js
+++ src/scripts/templates/cart.js
@@ -1,5 +1,14 @@
 import 'styles/templates/cart/index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const quantityInputs = document.querySelectorAll('[data-quantity-input]');
+  const quantitySubmitButton = document.querySelector('[data-quantity-submit]');
+
+  quantityInputs.forEach((input) => {
+    input.addEventListener('change', (event) => {
+        quantitySubmitButton.click();
+    });
+  });
+});
 
 window.addEventListener('load', () => {});
--- src/scripts/templates/product.js
+++ src/scripts/templates/product.js
@@ -1,5 +1,12 @@
 import 'styles/templates/product/index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const placeholder = document.getElementById('product-option-value-placeholder');
+  const option = document.getElementById('product-option-value');
+
+  option.addEventListener('change', (event) => {
+    placeholder.innerHTML = event.target.options[event.target.selectedIndex].text;
+  });
+});
 
 window.addEventListener('load', () => {});

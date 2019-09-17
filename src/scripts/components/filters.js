8d66d8c17fc8fac5cdc92ca6368bd93c0d36c5a2
Vladimir <vladimir.skok@bvaccel.com>
2019-09-17 16:53:30 -0700

    feat: add window resize event handling for filters

--- src/scripts/components/filters.js
+++ src/scripts/components/filters.js
@@ -1,6 +1,9 @@
 import dom from '../common/Dom';
+import { throttle } from '../common/Helpers';
 
+const throttleDuration = 250;
 const transitionDuration = 250;
+const breakpoint = 993;
 let expandedEl = null;
 
 const $elements = $(dom.collectionFiltersContainer);
@@ -27,7 +30,19 @@ const shrink = () => {
   expandedEl = null;
 }
 
+const isMobile = () => window.innerWidth < breakpoint;
+
+const windowResize = () => {
+  console.log('windowResize');
+  if (!isMobile()) {
+    console.log('isMobile - ', isMobile())
+    $(dom.collectionFilters).removeClass('is-expanded');
+  }
+}
+
 export const bindUIActions = () => {
+  $(window).on('resize', throttle(windowResize, throttleDuration));
+
   $(dom.collectionFilters).on('click', event => {
     const $target = $(event.target);
     const attr = $target.attr(dom.collectionFiltersTitleAttr);

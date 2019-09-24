59b623922c3b5dd8db0146af22746bcb47465b94
Vladimir <vladimir.skok@bvaccel.com>
2019-09-23 17:46:27 -0700

    feat: add simple product template

--- src/scripts/templates/product.js
+++ src/scripts/templates/product.js
@@ -1,5 +1,38 @@
 import 'styles/templates/product/index.scss';
+import {
+  update as updateInlineCart,
+  show as showInlineCart,
+  hide as hideInlineCart,
+} from '../components/inline-cart'
 
-document.addEventListener('DOMContentLoaded', () => {});
+const tease = () => {
+  showInlineCart();
+
+  setTimeout(() => {
+    hideInlineCart();
+  }, 2500);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#add-to-cart').on('click', () => {
+      $.post('/cart/add.js', {
+        quantity: 1,
+        id: '8456013938739'
+      })
+      .done(res => updateInlineCart());
+
+      tease();
+  });
+
+  $('#add-to-cart-2').on('click', () => {
+    $.post('/cart/add.js', {
+      quantity: 1,
+      id: '17461984722995'
+    })
+    .done(res => updateInlineCart());
+
+    tease();
+  });
+});
 
 window.addEventListener('load', () => {});

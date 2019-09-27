04eff3672a920053d721013d189420b03d16f7c3
Vladimir <vladimir.skok@bvaccel.com>
2019-09-26 18:19:19 -0700

    refactor: inline cart component

--- src/scripts/components/inline-cart.js
+++ src/scripts/components/inline-cart.js
@@ -10,103 +10,51 @@ const productUpdate = (variantId, quantity) => {
 
   $.post('/cart/update.js', {
     updates
-  });
-}
-
-const handleExpand = () => {
-  $(dom.lineItemEntryPoint).on('click', event => {
-    const target = event.target;
-
-    if (
-      target.matches('svg') ||
-      target.matches('path') ||
-      target.matches(dom.cartLineItemExpand)
-    ) {
-      $(target)
-        .closest(dom.cartLineItem)
-        .find(dom.cartLineItemEditContainer)
-        .toggleClass('is-active')
-    }
-  });
-}
-
-const handleRemove = () => {
-  $(dom.lineItemEntryPoint).on('click', event => {
-    const target = event.target;
-    console.log('remove', target)
-
-    if (target.matches('[data-cart-line-item-remove]')) {
-      const id = $(target).attr('data-variant-id')
-
-      console.log('remove id', id)
-      productUpdate(id, 0);
-      $(target).closest('[data-cart-line-item]').remove();
-    }
-  });
-}
-
-const handleQty = () => {
-  $(dom.lineItemEntryPoint).on('click', event => {
-    const target = event.target;
-
-    const id = $(target)
-      .closest('[data-cart-line-item-product-variant-id]')
-      .attr('data-cart-line-item-product-variant-id');
-
-    const qtyCounter = $(target)
-      .closest('[data-cart-line-item]')
-      .find('[data-cart-line-item-qty]');
-
-    const inlineCartCount = $(dom.inlineCartCount).html();
-
-    if (target.matches(dom.cartLineItemEditPlus)) {
-      const qty = new Number($(target).prev().html()) + 1;
-
-      productUpdate(id, qty);
-      // update edit counter
-      $(target).prev().html(qty);
-      // update line item counter
-      $(qtyCounter[0]).html(`QTY: ${qty}`);
-      // update total counter
-      $(dom.inlineCartCount).html(parseInt(inlineCartCount) + 1)
-    }
-
-    if (target.matches(dom.cartLineItemEditMinus)) {
-      const current = new Number($(target).next().html());
-
-      if (current > 1) {
-        const qty = current - 1;
-
-        productUpdate(id, qty);
-        // update edit counter
-        $(target).next().html(qty);
-        // update line item counter
-        $(qtyCounter[0]).html(`QTY: ${qty}`);
-        // update total counter
-        $(dom.inlineCartCount).html(parseInt(inlineCartCount) - 1)
-      } else {
-        productUpdate(id, 0);
-        $(target).closest('[data-cart-line-item]').remove();
-      }
-    }
   })
+  .done(() => update());
 }
 
 export const update = () => {
   $.get('/cart?view=contents', response => {
     $(dom.lineItemEntryPoint).html(response);
-  }).done(() => {
-    handleExpand();
-    handleQty();
-    handleRemove();
   });
 }
 
 const bindUIActions = () => {
-  update();
+  $(document).on('click', dom.cartLineItemRemove, event => {
+    const $this = $(event.currentTarget);
+    const id = $this.attr('data-variant-id')
+
+    console.log('remove id', id)
+    productUpdate(id, 0);
+    $this.closest(dom.cartLineItem).remove();
+  });
+
+  $(document).on('click', dom.cartLineItemExpand, event => {
+    const $this = $(event.currentTarget);
+      $this
+        .closest(dom.cartLineItem)
+        .find(dom.cartLineItemEditContainer)
+        .toggleClass('is-active')
+  });
+
+  $(document).on('click', dom.cartLineItemEditQty, event => {
+    const $this = $(event.currentTarget);
+    const $item = $this.closest(dom.cartLineItem)
+    const qtyUpdate = parseInt($this.attr('data-cart-line-item-edit-qty'));
+    const currentQty = parseInt($item.find(dom.cartLineItemQtyVal).text());
+    const qty = currentQty + qtyUpdate;
+    const id = $this.closest(dom.cartLineItemProductVariant)
+                    .attr('data-cart-line-item-product-variant-id');
+
+    productUpdate(id, qty);
+  });
 
   $(dom.inlineCartToggle).on('click', () => $(dom.inlineCart).toggleClass('is-active'));
   $(dom.inlineCartClose).on('click', hide);
 }
 
-export const init = () => bindUIActions();
+export const init = () => {
+  update();
+  bindUIActions();
+}
